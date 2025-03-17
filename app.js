b44d8bf06cbdd036d7128bf343824187f727b246
ArzuVar <dvamonroy@gmail.com>
2025-03-16 22:11:13 -0300

    Agregue comentarios y un README

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-
+//se ejecuta el evento cuando el documento se ha cargado
 document.addEventListener("DOMContentLoaded", () => {
-    const amigos = [];
+    const amigos = []; //arreglo que almacena los amigos
     
-    const inputAmigo = document.getElementById("amigo");
-    const listaAmigos = document.getElementById("listaAmigos");
-    const resultado = document.getElementById("resultado");
+    const inputAmigo = document.getElementById("amigo"); //input donde se ingresa el nombre del amigo
+    const listaAmigos = document.getElementById("listaAmigos"); //lista de amigos
+    const resultado = document.getElementById("resultado"); //resultado del sorteo
 
+//se agrega un evento al boton de agregar amigo
     document.getElementById("btnAdicionar").addEventListener("click", () => {
         agregarAmigo(inputAmigo.value.trim());
     });
-
+//se agrega un evento al boton de sortear amigo
     document.getElementById("btnSortear").addEventListener("click", sortearAmigo);
 
-    //funcion que agrega amigos
-
-
+//funcion que agrega amigos a la lista
     function agregarAmigo(nombre) {
-        if (!nombre) {
-            alert("Por favor, ingrese un nombre válido.");
+        if (!nombre) { //si no se ingresa un nombre
+            alert("Por favor, ingrese un nombre válido."); //se muestra un mensaje de alerta
             return;
         }
 
-        amigos.push(nombre);
-        actualizarLista();
-        limpiarInput();
+        amigos.push(nombre); //se agrega el nombre al arreglo de amigos
+        actualizarLista(); //se actualiza la lista de amigos
+        limpiarInput(); //se limpia el input
     }
-
+//funcion que actualiza la lista de amigos
     function actualizarLista() {
-        listaAmigos.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+        listaAmigos.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join(""); //se recorre el arreglo de amigos y se agrega a la lista
     }
 
     function sortearAmigo() {
